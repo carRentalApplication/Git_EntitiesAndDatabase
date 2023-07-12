3eaeb232a8a0abed50197bb62870b1f906e4097a
Ankush Raj <ankushchourasiya1601@gmail.com>
2023-07-12 17:12:53 +0530

    AddedAuthStatusModel
    AddedAuthStatusModel

---
+++ CarRental/CarRentalEntities/UpdateAuthStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalEntities
+{
+    public class UpdateAuthStatus
+    {
+        public Guid Id { get; set; }
+
+        public string Status { get; set; } = string.Empty;
+    }
+}
