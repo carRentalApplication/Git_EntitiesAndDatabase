5bfb71436d9bf060481f36f1584b9c8f104b0910
Ankush Raj <ankushchourasiya1601@gmail.com>
2023-07-15 17:49:12 +0530

    AddedNewEntity
    AddedNewEntity

---
+++ CarRental/CarRentalEntities/BookingModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalEntities
+{
+    public class BookingModel
+    {
+        public string TravallerName { get; set; }
+        public long TravallerNumber { get; set; }
+        public string PickUpDate { get; set; }
+        public string DropDate { get; set; }
+        public string PickUpAddress { get; set; }
+        public string PaymentMode { get; set; }
+        public double TotalAmount { get; set; }
+        public int VehicleId { get; set; }
+        public string userId { get; set; }
+    }
+}
